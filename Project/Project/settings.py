532ea14077e62e8dbee7d0df03692db9f6f2904c
PonchoCeniceros <gio.icc.uach@gmail.com>
2021-03-13 01:27:57 -0700

    [AI-13] pickle dataset has been loaded

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4*s=bd%-gh80ej)q7w*jc&d^*#fvpzj_-e@edgv^as%l764nzn'
-
+SECRET_KEY = '4*s=bd%-gh80ej)q7w*jc&d^*#fvpzj_-e@edgv^as%l764nzn' #config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db/db.sqlite3',
     }
 }
 
